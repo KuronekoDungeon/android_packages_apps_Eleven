1f7758548715c0ef2afe5f4cf5890ec262c2ecad
maz-1 <ohmygod19993@gmail.com>
2016-06-11 12:44:38 +0800

    Fix build errors for aosp

--- src/com/cyanogenmod/eleven/utils/BitmapWithColors.java
+++ src/com/cyanogenmod/eleven/utils/BitmapWithColors.java
@@ -19,7 +19,7 @@ import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.os.Looper;
 import android.support.v7.graphics.Palette;
-import android.support.v7.graphics.Target;
+//import android.support.v7.graphics.Target;
 import android.util.LruCache;
 
 public class BitmapWithColors {
